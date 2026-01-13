b427fea30f46a18c34ae366dd2bfa6b0bc1fb1e2
DharaCS23181 <dhuvaviyadhara.cse23@sbjit.edu.in>
2026-01-13 18:12:32 +0530

    header and footer Dhara

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header-Dhara.jsx";
+import Footer from "./components/footer-Dhara.jsx";
+// Pages
+// import Home from "./home.jsx";
+
+
+export default function App() {
   return (
-    <div>
-      {/* React application starts here */}
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* other routes later */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
-
-export default App;
